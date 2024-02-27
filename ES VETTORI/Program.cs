640218f42e7b2aa8576557f5dc1a62be9c661f82
39327 <39327@LAPTOP-RNB7N71H>
2024-02-27 23:26:07 +0100

    Aggiungere i file di progetto.

---
+++ ES VETTORI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES_VETTORI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1=new Vector(5,6);
+            Vector v2 = new Vector(4, 7);
+            double n = 3;
+            double modulo = Vector.Modulo(v1);
+
+
+            Console.WriteLine("{0}; {1};{2};{3});",v1 + v2, v1 - v2, v1 * v2, v1 * n);
+                Console.WriteLine("{0};{1};{2},{3}", n*v1,v1/n, +v1, -v1);
+            Console.WriteLine("{0}", modulo);
+
+           Console.ReadLine();
+
+            
+
+        }
+    }
+}
---
+++ ES VETTORI/Vector.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES_VETTORI
+{
+    public class Vector
+    {
+        public readonly double X;
+        public readonly double Y; 
+        public Vector(double x, double y)    
+        {
+            X = x;
+            Y = y;  
+        }
+        public static double Modulo (Vector v1)
+        {
+            return Math.Sqrt((v1.X * v1.X)+( v1.Y * v1.Y));
+
+        }
+        public static Vector operator +(Vector v1, Vector v2) 
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y); 
+
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+
+        }
+
+        public static Vector operator *(Vector v1, Vector v2) 
+        {
+            return new Vector(v1.X * v2.X, v1.Y * v2.Y);
+        
+        }
+
+        public static Vector operator *(Vector v1, double s)
+        {
+            return new Vector(v1.X * s, v1.Y * s);
+
+        }
+
+        public static Vector operator *(double s, Vector v1)
+        {
+            return new Vector(s*v1.X, s*v1.Y);
+
+        }
+
+        public static Vector operator /(Vector v1, double s)
+        {
+            return new Vector(v1.X /s, v1.Y / s);
+
+        }
+        public static Vector operator +(Vector v1)
+        {
+            return new Vector(+v1.X, + v1.Y );
+
+        }
+        public static Vector operator -(Vector v1)
+        {
+            return new Vector(-v1.X, -v1.Y);
+
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+
+
+
+
+    }
+}
